6ff59a861de3aea9bd4d6e11b160b5896b6da429
tlasalle5027 <tlasalle5027@gmail.com>
2020-11-02 11:32:25 -0500

    SearchBar and BusinessList components into App

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello CodeCademy.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList /> 
     </div>
   );
 }
