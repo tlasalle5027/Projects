ca776842be928e75618bd132adae2182e8889262
tlasalle5027 <tlasalle5027@gmail.com>
2020-11-04 22:44:22 -0500

    Added Search functionality/states

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -24,14 +24,23 @@ const businesses = [
   business
 ];
 
-function App() {
-  return (
-    <div class="App">
-      <h1>ravenous</h1>
-        <SearchBar />
-        <BusinessList businesses={businesses} /> 
-    </div>
-  );
+class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  
+  }
+  
+  render() {
+    return (
+      <div class="App">
+        <h1>ravenous</h1>
+          <SearchBar searchYelp={this.searchYelp} />
+          <BusinessList businesses={businesses} /> 
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
