15cc9dc90365950b8cf9b11b3062848c810ebdcb
tlasalle5027 <tlasalle5027@gmail.com>
2020-11-09 12:41:28 -0500

    Created Playlist name change functionality

--- jammming/src/components/App/App.js
+++ jammming/src/components/App/App.js
@@ -35,6 +35,7 @@ class App extends React.Component {
 
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
   
   addTrack(track){
@@ -54,6 +55,11 @@ class App extends React.Component {
     
     this.setState({ playlistTracks: newPlaylist });
   }
+
+  updatePlaylistName(name){
+    this.setState({ playlistName: name });
+  }
+
   
   render(){
     return (
@@ -66,7 +72,8 @@ class App extends React.Component {
                            onAdd={this.addTrack}/>
             <Playlist playlistName={this.state.playlistName}
                       playlistTracks={this.state.playlistTracks}
-                      onRemove={this.removeTrack}/>
+                      onRemove={this.removeTrack}
+                      onNameChange={this.updatePlaylistName}/>
           </div>
         </div>
       </div>
