f311d154ed8493c6d398ed3312da785053117885
tlasalle5027 <tlasalle5027@gmail.com>
2020-11-10 12:52:54 -0500

    Finished Save Playlist functionality

--- jammming/src/components/App/App.js
+++ jammming/src/components/App/App.js
@@ -47,6 +47,14 @@ class App extends React.Component {
     const trackUris = this.state.playlistTracks.map(track =>
       track.uri);
 
+      Spotify.savePlaylist(this.state.playlistName, trackUris)
+        .then(() => {
+          this.setState({
+            playlistName: 'New Playlist',
+            playlistTracks: [],
+          })
+        });
+
   }
 
   search(term){
