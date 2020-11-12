0ef9404e3515f36bc11a65f84e49014015984e0f
tlasalle5027 <tlasalle5027@gmail.com>
2020-11-12 14:34:31 -0500

    Added Enter keyPress to search Prepared for new build (jammming)

--- jammming/src/components/SearchBar/SearchBar.js
+++ jammming/src/components/SearchBar/SearchBar.js
@@ -9,6 +9,7 @@ class SearchBar extends React.Component{
 
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     search(){
@@ -19,11 +20,18 @@ class SearchBar extends React.Component{
         this.setState({ term: e.target.value });
     }
 
+    handleKeyPress(e){
+        if(e.keyCode === 13 && this.state.term ){
+            this.search();
+        }
+    }
+
     render(){
         return(
             <div className="SearchBar">
                 <input placeholder="Enter A Song, Album, or Artist" 
-                       onChange={this.handleTermChange}/>
+                       onChange={this.handleTermChange}
+                       onKeyUp={this.handleKeyPress}/>
                 <button className="SearchButton"
                         onClick={this.search}>SEARCH</button>
             </div>
