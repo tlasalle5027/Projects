f89530ff27333369197cd91e30c824ca49c6cb62
tlasalle5027 <tlasalle5027@gmail.com>
2020-11-17 13:57:03 -0500

    Finished writing minions routes

--- project-4-boss-machine-start/server/api.js
+++ project-4-boss-machine-start/server/api.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+const minionRouter = require('./minions');
+
+apiRouter.use('/minions', minionRouter);
+
 
 
 module.exports = apiRouter;
---
+++ project-4-boss-machine-start/server/minions.js
@@ -0,0 +1,48 @@
+const minionRouter = require('express').Router();
+
+const { 
+    addToDatabase,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+} = require('./db');
+
+minionRouter.get('/', (req, res, next) => {
+    res.send(getAllFromDatabase('minions'));
+});
+
+minionRouter.post('/', (req, res, next) => {
+    const newMinion = addToDatabase('minions', req.body);
+    res.status(201).send(newMinion);
+});
+
+minionRouter.get('/:minionid', (req, res, next) => {
+    const minion = getFromDatabaseById('minions', req.params.minionid);
+    if(minion){
+        res.send(minion);
+    } else {
+        res.status(404).send();
+    }
+});
+
+minionRouter.put('/:minionid', (req, res, next) => {
+    const minion = updateInstanceInDatabase('minions', req.body);
+    if(minion){
+        res.send(minion);
+    } else {
+        res.status(404).send();
+    }
+});
+
+minionRouter.delete('/:minionid', (req, res, next) => {
+    const deleted = deleteFromDatabasebyId('minions', req.params.minionid);
+    if(deleted){
+        res.status(204).send();
+    } else {
+        res.status(404).send();
+    }
+})
+
+
+module.exports = minionRouter;
