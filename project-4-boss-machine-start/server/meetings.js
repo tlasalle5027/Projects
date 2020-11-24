428a472ecbe83cd5ec5d158652e9dfcac2f52b20
tlasalle5027 <tlasalle5027@gmail.com>
2020-11-24 13:46:50 -0500

    Finished writing meetingRouter

--- project-4-boss-machine-start/server/meetings.js
+++ project-4-boss-machine-start/server/meetings.js
@@ -2,12 +2,28 @@ const meetingRouter = require('express').Router();
 
 const { 
     addToDatabase,
+    createMeeting,
+    deleteAllFromDatabase,
     getAllFromDatabase,
-    getFromDatabaseById,
-    updateInstanceInDatabase,
-    deleteFromDatabasebyId,
 } = require('./db');
 
+meetingRouter.get('/', (req, res, next) => {
+    res.send(getAllFromDatabase('meetings'));
+});
 
+meetingRouter.post('/', (req, res, next) => {
+    const newMeeting = addToDatabase('meetings', createMeeting());
+    if(newMeeting){
+        res.status(201).send(newMeeting);
+    } else {
+        res.status(404).send();
+    }
+
+});
+
+meetingRouter.delete('/', (req, res, next) => {
+    deleteAllFromDatabase('meetings');
+    res.status(204).send();
+});
 
 module.exports = meetingRouter;
