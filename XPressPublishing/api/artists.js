c387c649b2a33cf0ca581a43c390a55f9e5d92a4
tlasalle5027 <tlasalle5027@gmail.com>
2020-12-01 16:07:44 -0500

    Wrote Artist PUT route

--- XPressPublishing/api/artists.js
+++ XPressPublishing/api/artists.js
@@ -45,7 +45,7 @@ artistRouter.post('/', (req, res, next) => {
     const isCurrentlyEmployed = req.body.artist.isCurrentlyEmployed === 0 ? 0 : 1;
 
     if(!name || !dateOfBirth || !biography ){
-        res.sendStatus(400);
+        return res.sendStatus(400);
     }
 
     const sql = 'INSERT INTO Artist (name, date_of_birth, biography, is_currently_employed) VALUES ($name, $dateOfBirth, $biography, $isCurrentlyEmployed)';
@@ -59,16 +59,45 @@ artistRouter.post('/', (req, res, next) => {
     db.run(sql, values, function(err) {
         if(err){
             next(err);
+        } else {            
+            db.get(`SELECT * FROM Artist WHERE id=${this.lastID}`, function(err, artist){
+                res.status(201).send({artist: artist});
+            });
         }
-
-        db.get(`SELECT * FROM Artist WHERE id=${this.lastID}`, function(err, artist){
-            res.status(201).send({artist: artist});
-        });
     });
 });
 
 artistRouter.put('/:artistId', (req, res, next) => {
 
+    const name = req.body.artist.name;
+    const dateOfBirth = req.body.artist.dateOfBirth;
+    const biography = req.body.artist.biography;
+    const isCurrentlyEmployed = req.body.artist.isCurrentlyEmployed === 0 ? 0 : 1;
+
+    if (!name || !dateOfBirth || !biography) {
+        return res.sendStatus(400);
+     }
+
+    const sql = 'UPDATE Artist SET name = $name, date_of_birth = $dateOfBirth, biography = $biography, is_currently_employed = $isCurrentlyEmployed WHERE Artist.id = $artistId';
+    const values = {
+        $name: name,
+        $dateOfBirth: dateOfBirth,
+        $biography: biography,
+        $isCurrentlyEmployed: isCurrentlyEmployed,
+        $artistId: req.params.artistId
+    };
+ 
+    db.run(sql, values, (err) => {
+        if(err){
+            next(err);
+        } else {
+            db.get(`SELECT * FROM Artist WHERE id=${req.params.artistId}`, function(err, artist){
+                res.status(200).send({artist: artist});
+            });
+        }
+
+    });    
+    
 });
 
 module.exports = artistRouter;
