b589254dd2e2d25f937cf3f78e0a1eb6dab47e02
tlasalle5027 <tlasalle5027@gmail.com>
2020-12-02 11:42:08 -0500

    Added the Issue table creation

--- XPressPublishing/migration.js
+++ XPressPublishing/migration.js
@@ -3,8 +3,9 @@ const sqlite3 = require('sqlite3');
 //Open the provided database
 const db = new sqlite3.Database('./database.sqlite');
 
-//Create an Artist Table if it does not exist
+//Table creation
 db.serialize(() => {
+    //Create the Artist Table
     db.run("DROP TABLE IF EXISTS Artist");
     db.run('CREATE TABLE IF NOT EXISTS `Artist` ( ' +
     '`id` INTEGER NOT NULL, ' +
@@ -13,11 +14,27 @@ db.serialize(() => {
     '`biography` TEXT NOT NULL, ' +
     '`is_currently_employed` INTEGER NOT NULL DEFAULT 1, ' +
     'PRIMARY KEY(`id`) )');
+
+    //Create the Series Table
     db.run("DROP TABLE IF EXISTS Series");
     db.run('CREATE TABLE IF NOT EXISTS `Series` ( ' + 
     '`id` INTEGER NOT NULL, ' + 
     '`name` TEXT NOT NULL,' + 
     '`description` TEXT NOT NULL,' + 
     ' PRIMARY KEY(`id`) )');
+
+    //Create the Issue Table
+    db.run("DROP TABLE IF EXISTS Issue");
+    db.run("CREATE TABLE IF NOT EXISTS `Issue` ( " + 
+    '`id` INTEGER NOT NULL, ' + 
+    '`name` TEXT NOT NULL, ' + 
+    '`issue_number` INTEGER NOT NULL, ' + 
+    '`publication_date` TEXT NOT NULL, ' + 
+    '`artist_id` INTEGER NOT NULL, ' + 
+    '`series_id` INTEGER NOT NULL, ' + 
+    'PRIMARY KEY(`id`), ' + 
+    'FOREIGN KEY(`artist_id`) REFERENCES Artist(`id`), ' + 
+    'FOREIGN KEY(`series_id`) REFERENCES Series(`id`) )');
+
 
 });
