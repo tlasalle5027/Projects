37cc3f70e1615fab466bc647c3dd2b1d008597fe
tlasalle5027 <tlasalle5027@gmail.com>
2020-12-04 13:46:10 -0500

    Wrote game logic

--- NumberGuesser/script.js
+++ NumberGuesser/script.js
@@ -2,5 +2,55 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+/*
+* generateTarget generates the target number
+* with which the players must guess.
+*/
+const generateTarget = function(){
+    return Math.floor(Math.random() * 9);
+}
 
+/*
+* getAbsoluteDistance gets the distance between
+* guess and target as an absolute number
+*/
+const getAbsoluteDistance = function(guess, target){
+    return Math.abs(target - guess);
+}
+
+/*
+* compareGuesses checks to see which guess is closest
+* to the target number
+*/
+const compareGuesses = function(humanGuess, compGuess, target){
+
+    const humanDist = getAbsoluteDistance(humanGuess, target);
+    const compDist = getAbsoluteDistance(compGuess, target);
+    
+    if(humanDist < compDist){
+        return true;
+    } else if(humanDist > compDist){
+        return false;
+    } else {
+        return true;
+    }        
+    
+}
+
+/*
+* updateScore updates the winners score
+*/
+const updateScore = function(winner){
+    if(winner === 'human'){
+        humanScore++;
+    } else {
+        computerScore++;
+    }
+}
+
+/*
+* advanceRound advances the round number
+*/
+const advanceRound = function(){
+    currentRoundNumber++;
+}
